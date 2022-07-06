081f07835737ae9d9fe99ade34bee083a4724633
Matheus Pimentel <matheuspimentel.pimentel@gmail.com>
2022-07-06 16:05:02 -0300

    Limpando código

--- Raspagem_de_dados_Preco_de_soja_e_milho/app.py
+++ Raspagem_de_dados_Preco_de_soja_e_milho/app.py
@@ -2,75 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 requestsDoSite = requests.get("https://agrural.com.br/precossojaemilho/")
-#print(requestsDoSite)
-
 
 soup = BeautifulSoup(requestsDoSite.text, 'html.parser')
 
-data = ""
-praca = []; compra = []; dia = []; semana = []; mes = []
-
-i = 2
-
-'''
-for td in soup.find_all('td'):
-    i+=1
-    #print(i)
-    if(i == 2):
-        #print(td.get_text())
-        data = td.get_text()
-    if(i == 3):        
-        praca.append(td.get_text())
-        print(td.get_text())        
-
-    if(i == 4):
-        praca.append(td.get_text())
-        print(td.get_text())
-
-    if(i == 5):
-        praca.append(td.get_text())
-        print(td.get_text())
-
-    if(i == 6):
-        praca.append(td.get_text())
-        print(td.get_text())
-
-    if(i == 7):
-        praca.append(td.get_text())
-        print(td.get_text())
-        i=3
-
-    i+=1'''
-
 tds = []
 
-estados_soja = []; praca_soja = []; compra_soja = []; dia_soja = []; semana_soja = []; mes_soja = []
+data_soja = ""; estados_soja = []; praca_soja = []; compra_soja = []; dia_soja = []; semana_soja = []; mes_soja = []
 
 for td in soup.find_all('td'):    
        tds.append(td.text)
 
 def soja():
-    data = tds[1]
-    print(data)
-
-    i=0; c = 1; max = 190
+    #Pegando data de cotação
+    data_soja = tds[1]
+    print("Soja – mercado disponível: " + data_soja)
     
-
-    for dadoTd in range(len(tds)):    
+    i=0; c = 1; max = 190; e = ""
+    
+    for t in range(len(tds)) :    
 
         if((i > 7) and (i <= max)):
-
+            
             #Pegando as siglas dos estados
             if((len(tds[i]) == 2) and (tds[i] !=  int)):
-                estados_soja.append(tds[i])       
-                print(tds[i])
+                estados_soja.append(tds[i])                
+                e = tds[i]                      
 
             #Pegando praçca, compra, dia, semana, mes
             elif((len(tds[i]) >= 2) and (tds[i] !=  int)):
-                
+
                 if(c == 1):
                     praca_soja.append(tds[i])
-                    print("Praça : " + tds[i])
+                    print("Praça : " + e + " - "+ tds[i])
                     c = 2
                 elif(c == 2):
                     compra_soja.append(tds[i])
